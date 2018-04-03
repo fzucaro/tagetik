aeebb384264c1f551bfa5482bc779e193485f14f
Francesco Zucaro <francesco.zucaro@waveinformatica.com>
2018-04-03 17:18:00 +0200

    Allineamento flussi domini

---
+++ Tagetik_flusso_Domini/04-INSERT_T_TAGETIK_DOMINI.sql
@@ -0,0 +1,45 @@
+-- SK_F2.F2_D_Tipi_Operazioni
+INSERT INTO [SK_F2_FLUSSI].[F2_D_TabelleDominio]
+           ([Tabella]
+           ,[Campo_Rif]
+           ,[Campo_Output])
+     VALUES
+           ('SK_F2.F2_D_Tipi_Operazioni'
+           ,'ID'
+           ,'TIPOP')
+
+
+--  Tipo derivato
+INSERT INTO [SK_F2_FLUSSI].[F2_D_TabelleDominio]
+           ([Tabella]
+           ,[Campo_Rif]
+           ,[Campo_Output])
+     VALUES
+           ('SK_F2.F2_D_Tipi_Derivato'
+           ,'ID'
+           ,'TIPDER') 
+
+--[SK_F2].[F2_D_Tipologie_Fondo]
+ INSERT INTO [SK_F2_FLUSSI].[F2_D_TabelleDominio]
+           ([Tabella]
+           ,[Campo_Rif]
+           ,[Campo_Output])
+     VALUES
+           ('SK_F2.F2_D_Tipologie_Fondo'
+           ,'ID'
+           ,'TIPF') 
+
+-- SK_F2.F2_D_Valuta
+ INSERT INTO [SK_F2_FLUSSI].[F2_D_TabelleDominio]
+           ([Tabella]
+           ,[Campo_Rif]
+           ,[Campo_Output])
+     VALUES
+           ('SK_F2.F2_D_Valuta'
+           ,'Codice_SWIFT'
+           ,'CURNCY') 
+
+
+
+
+
