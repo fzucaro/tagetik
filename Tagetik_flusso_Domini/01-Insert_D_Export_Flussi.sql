dfcdcf83147a44d3c7249ce8208d42a3f431081c
Francesco Zucaro <francesco.zucaro@waveinformatica.com>
2018-04-03 17:10:56 +0200

    Allineamento flussi domini

--- Tagetik_flusso_Domini/01-Insert_D_Export_Flussi.sql
+++ Tagetik_flusso_Domini/01-Insert_D_Export_Flussi.sql
@@ -14,15 +14,15 @@ INSERT INTO [SK_F2].[F2_D_ExportFlussi]
            ,[Data_Inizio]
            ,[Data_Fine])
      VALUES
-           (35
-           ,'TagetikMov'
-           ,'Export flusso Tagetik Movimenti'
+           (NULL
+           ,'TagetikDom'
+           ,'Export flusso Tagetik Domini'
            ,'SK_F2_FLUSSI.F2_EXP_TAGETIK_MOV'
-           ,'TGTKMOV_{Data}'
+           ,'TGTKFLU_{Data}'
            ,'txt'
            ,1
            ,10
-           ,'snd_tgtk_mov.cmd'
+           ,'snd_tgtk_flu.cmd'
            ,getdate()
            ,NULL)
 GO
